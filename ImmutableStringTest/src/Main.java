932668b6c8fe2f2d43569f079af9b42d44415735
kywpcm <kywpcm@gmail.com>
2017-08-13 19:53:51 +0900

    immutable String object..

---
+++ ImmutableStringTest/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String str = "String is immutable object";
+
+		Main obj = new Main();
+		
+		System.out.println(str);
+		obj.getHashCode(str);
+		System.out.println(str);
+
+	}
+
+	public void getHashCode(String immStr) {
+		System.out.println("hash code : " + immStr.hashCode());
+		immStr = immStr + "111";
+		System.out.println("hash code : " + immStr.hashCode());
+		immStr = immStr + "222";
+		System.out.println("hash code : " + immStr.hashCode());
+	}
+
+}
