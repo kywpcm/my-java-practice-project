aa03d7417f60edd93bae1df7b7f637988747b4c6
kywpcm <kywpcm@gmail.com>
2017-10-23 00:21:05 +0900

    Signed-off-by: kywpcm <kywpcm@gmail.com>

--- MyAlgorithmSolve/src/main/Main.java
+++ MyAlgorithmSolve/src/main/Main.java
@@ -11,22 +11,30 @@ public class Main {
 	public static void main(String[] args) {
 
 		int comNum = 0;
-
-		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-			comNum = Integer.parseInt(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			comNum = Integer.parseInt(br.readLine()); // 입력
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("comNum : " + comNum);
 		
+		String comStr = "";
 		if (comNum > 0) {
 			
 			for (int i = 0; i < comNum; i++) {
+				
+				try {
+					comStr = br.readLine(); // 입력
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				// test
-				System.out.println("i : " + i);
+				System.out.println("comStr : " + comStr);
 				
+				// 명령어 파싱..
 				
+				// 스택 클래스 서비스 호출..
 			}
 			
 		} else {
