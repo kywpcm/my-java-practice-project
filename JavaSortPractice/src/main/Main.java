17e9d5e27e2a8c1de3f23b9faf59e90baf93270e
kywpcm <kywpcm@gmail.com>
2018-04-16 18:21:35 +0900

    JavaSortPractice 추가

---
+++ JavaSortPractice/src/main/Main.java
@@ -0,0 +1,67 @@
+package main;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("banana", "apple", "cup", "egg", "fire~", "desk");
+        if (list instanceof ArrayList)
+            System.out.println("ArrayList");
+        else if (list instanceof List)
+            System.out.println("List");
+
+        /* List to Array */
+        // 1.
+        String[] array = (String[]) list.toArray();
+        // 2.
+        // param 배열의 형태로 런타임시에 리스트를 새로운 배열에 dump
+        // 새로운 배열의 size는 param 배열의 size와 리스트의 size로 결정
+//        String[] array = list.toArray(new String[0]);
+
+        System.out.println();
+
+
+        /* Collections 클래스 sort  */
+        System.out.println("list before sort : " + list);
+        Collections.sort(list, new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2); // ascending order
+//                return o2.compareTo(o1); // descending order
+            }
+        });
+        /* List 인터페이스 구현체들의 sort (since 1.8) */
+        list.sort(new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+//                return o1.compareTo(o2); // ascending order
+                return o2.compareTo(o1); // descending order
+            }
+        });
+        System.out.println("sorted list : " + list);
+
+
+        System.out.println();
+
+
+        /* Arrrays 클래스 sort */
+        for (String item : array)
+            System.out.println("array before sort : " + item);
+        Arrays.sort(array, new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2); // ascending order
+//                return o2.compareTo(o1); // descending order
+            }
+        });
+        for (String item : array)
+            System.out.println("sorted array : " + item);
+
+    }
+
+}
