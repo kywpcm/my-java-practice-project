6af9fbd0aea402be499abf8bf4446adef99b5d0f
kywpcm <kywpcm@gmail.com>
2018-08-23 17:39:06 +0900

    N/A

--- MyAlgorithmSolve/src/goorm/_01_bm_problem/Problem03.java
+++ MyAlgorithmSolve/src/goorm/_01_bm_problem/Problem03.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Problem03 {
 
@@ -28,52 +26,56 @@ public class Problem03 {
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input1 = null;
-        String input2 = null;
-        String input3 = null;
-        String inputEnd = null;
+        List<String> list = new ArrayList<>();
         try {
-            input1 = br.readLine();
-            input2 = br.readLine();
-            input3 = br.readLine();
-            inputEnd = br.readLine();
+            while (true) {
+                String input = br.readLine();
+                if (input.equals("END")) break;
+                list.add(input);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        List<String> list = new ArrayList<>();
-        list.add(input1);
-        list.add(input2);
-        list.add(input3);
+        // test
+        for (String str : list)
+            System.out.println("str : " + str);
 
         // String patternStr = "(19|20)\\d{2}[- /.]*(0[1-9]|1[012])[- /.]*(0[1-9]|[12][0-9]|3[01])"; // 날자를 패턴으로 지정
 
-        Pattern p = Pattern.compile("(20)?\\d{2}[/\\-년]+(0?[1-9]|1[012])[/\\-월]+(0?[1-9]|[12][0-9]|3[01])[일]?");
-        List<String> dateList = new ArrayList<>();
-        // 입력 순서대로
-        for (int i = 0; i < 3; i++) {
-            Matcher m = p.matcher(list.get(i));
-            String date = "";
-            if (m.find()) {
-                date = m.group();
-                String[] splitedStr = date.split("[/|\\-|[년월일]]");
-                if (splitedStr[0].length() == 2) {
-                    splitedStr[0] = "20" + splitedStr[0];
-                }
-                if (splitedStr[1].length() == 1) {
-                    splitedStr[1] = "0" + splitedStr[1];
-                }
-                if (splitedStr[2].length() == 1) {
-                    splitedStr[2] = "0" + splitedStr[2];
-                }
+//        Pattern p = Pattern.compile("(20)?\\d{2}[/\\-년]+(0?[1-9]|1[012])[/\\-월]+(0?[1-9]|[12][0-9]|3[01])[일]?");
+//        List<String> dateList = new ArrayList<>();
+//        // 입력 순서대로
+//        for (int i = 0; i < 3; i++) {
+//            Matcher m = p.matcher(list.get(i));
+//            String date = "";
+//            if (m.find()) {
+//                date = m.group();
+//                String[] splitedStr = date.split("[/|\\-|[년월일]]");
+//                if (splitedStr[0].length() == 2) { // 17, 18, ...
+//                    splitedStr[0] = "20" + splitedStr[0];
+//                }
+//                if (splitedStr[1].length() == 1) { // 1 ~ 9
+//                    splitedStr[1] = "0" + splitedStr[1];
+//                }
+//                if (splitedStr[2].length() == 1) { // 1 ~ 9
+//                    splitedStr[2] = "0" + splitedStr[2];
+//                }
+//
+//                dateList.add(splitedStr[0] + splitedStr[1] + splitedStr[2]);
+//            }
+//        }
+//
+//        // test
+//        for (String str : dateList)
+//            System.out.println(str);
 
-                dateList.add(splitedStr[0] + splitedStr[1] + splitedStr[2]);
-            }
+
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        // test
-        for (String str : dateList)
-            System.out.println(str);
     }
 
 }
