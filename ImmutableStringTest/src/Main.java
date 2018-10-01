d16f88ac9e74853bf948efe5fcb90a8325a9a156
kywpcm <kywpcm@gmail.com>
2018-10-01 19:53:51 +0900

    n/a

--- ImmutableStringTest/src/Main.java
+++ ImmutableStringTest/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String str = "String is immutable object";
+		String str = "String is immutable object222";
 
 		Main obj = new Main();
 		
