dc390473324f88e2fa1a7006ba7870d61b4d7c19
kywpcm <kywpcm@gmail.com>
2019-01-04 16:09:59 +0900

    n/a

--- FileExtractor/ext_n_copy_onion.sh
+++ FileExtractor/ext_n_copy_onion.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # 특정 문자열 라인 포함한 원본 파일 복사
-find ./ -name "*.txt" -exec grep -l "홍길동" {} \; | awk '{print "cp "$1" ../out/source_file_list/"}' | sh
+find ./ -name "*.txt" -exec grep -l "홍길동" '{}' \; | awk '{print "cp "$1" ../out/source_file_list/"}' | sh
 
 # 특정 문자열 라인(파일명 + 문자열) 추출해서 하나의 파일로 생성
 find ./ -name "*.txt" | xargs grep "홍길동" > ../out/out.txt
