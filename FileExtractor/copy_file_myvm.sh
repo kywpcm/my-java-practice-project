a1688bb8d68a4553db876ec66d558341dac79d34
kywpcm <kywpcm@gmail.com>
2019-02-13 17:39:53 +0900

    modify

--- FileExtractor/copy_file_myvm.sh
+++ FileExtractor/copy_file_myvm.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 while IFS= read -r LINE
 do
+  # 파일명 추출
   SRC_FILE=`awk '{
-      offset=match($0,":") - 1;
+      offset=match($0,":")-1;
       print substr($0,0,offset);
   }'`
   cp -u $SRC_FILE $1
